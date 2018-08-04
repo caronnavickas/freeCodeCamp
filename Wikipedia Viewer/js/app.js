08dff1b96713d4a992a87a3ab8a08ba37138c157
caronnavickas <caron.navickas@gmail.com>
2018-08-04 12:11:46 +1000

    Finish Logic
    - rearranged buttons in UI
    - updated ui experience in app.js
    - ensured edge case data lengths display correctly

--- Wikipedia Viewer/js/app.js
+++ Wikipedia Viewer/js/app.js
@@ -1,8 +1,28 @@
 var SEARCH_TERM_ID = '#searchTerm';
 
+function randomSearch() {
+    window.open('https://en.wikipedia.org/wiki/Special:Random', '_blank');
+}
+
 function enableSearchButton() {
-    $('#search').removeClass('disabled');
+    $('#search').removeClass('hide');
+}
+
+function hideSpinner() {
     $('#spinner').addClass('hide');
+}
+
+function enableClearButton() {
+    $('#clear').removeClass('hide');
+}
+
+function disableClearButton() {
+    $('#clear').addClass('hide');
+}
+
+function clearSearch() {
+    $('#searchTerm').val('');
+    $("#output").html("");
 }
 
 function generateColumn(cardContent, cardLink, cardTitle, i) {
@@ -43,15 +63,24 @@ function onKeyUp(e) {
     }
 }
 
+function outputCards(cards) {
+    $('#output').append(
+        `<div class="row">
+        ${cards}
+    </div>`
+    );
+}
+
 function doSearch() {
     var searchTerm = $(SEARCH_TERM_ID).val();
     var isValidSearch = IsValidSearch(searchTerm);
 
     if (isValidSearch) {
         // Control UI
-        $('#search').addClass('disabled');
+        $('#search').addClass('hide');
         $('#spinner').removeClass('hide');
         $("#output").html(""); // clear content from earlier searches
+        enableClearButton();
 
         // Do Work
         var encodedSearchTerm = encodeURIComponent(searchTerm);
@@ -104,14 +133,17 @@ function doSearch() {
             dataType: 'jsonp',
             contentType: 'application/javascript',
             success: function (data) {
-                console.log('do we have data? ', data);
                 $("#output").html(""); // clears out contents from earlier searched
                 $("#output").append("<h5>Click to open in Wikipedia:</h5>")
 
                 var c = 0;
-                var cards = '';
                 var card = '';
-                for (var i = 0; i < data[1].length; i++) {
+                var cards = '';
+                var l = data[1].length;
+                var t = 3;
+                var m = l % t;
+
+                for (var i = 0; i < l; i++) {
                     var cardContent = data[2][i];
                     var cardLink = data[3][i];
                     var cardTitle = data[1][i];
@@ -120,43 +152,48 @@ function doSearch() {
                     cards = `${cards}${card}`;
                     c++;
 
-                    if (c === 3) {
-                        $('#output').append(
-                            `<div class="row">
-                                ${cards}
-                            </div>`
-                        );
-                        cards = '';
-                        c = 0;
+                    if (i < (l - m)) {
+                        if (c === t) {
+                            outputCards(cards);
+                            cards = '';
+                            c = 0;
+                        }
+                    } else {
+                        if (c === m) {
+                            outputCards(cards);
+                        }
                     }
                 }
 
-                $.ajax({
-                    url: buildApiDetail,
-                    method: "GET",
-                    dataType: "jsonp",
-                    success: function (data) {
-                        for (var i = 0; i < data.query.pages.length; i++) {
-                            if (data.query.pages[i].hasOwnProperty("thumbnail")) {
-                                //     $("#image" + (data.query.pages[i].index - 1)).html(`src='${data.query.pages[i].thumbnail.source}' class='responsive-img valign'>`);
-                                $("#image" + (data.query.pages[i].index - 1)).attr('src', data.query.pages[i].thumbnail.source);
-                            } else {
-                                // $("#image" + (data.query.pages[i].index - 1)).html("<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Article_icon_cropped.svg/512px-Article_icon_cropped.svg.png' class='responsive-img valign articleIcon'>");
-                                $("#image" + (data.query.pages[i].index - 1)).attr('src', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Article_icon_cropped.svg/512px-Article_icon_cropped.svg.png');
+                if (l > 0) {
+                    // make sure that we have data to search images for...
+                    $.ajax({
+                        url: buildApiDetail,
+                        method: "GET",
+                        dataType: "jsonp",
+                        success: function (data) {
+                            for (var i = 0; i < data.query.pages.length; i++) {
+                                if (data.query.pages[i].hasOwnProperty("thumbnail")) {
+                                    //     $("#image" + (data.query.pages[i].index - 1)).html(`src='${data.query.pages[i].thumbnail.source}' class='responsive-img valign'>`);
+                                    $("#image" + (data.query.pages[i].index - 1)).attr('src', data.query.pages[i].thumbnail.source);
+                                } else {
+                                    // $("#image" + (data.query.pages[i].index - 1)).html("<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Article_icon_cropped.svg/512px-Article_icon_cropped.svg.png' class='responsive-img valign articleIcon'>");
+                                    $("#image" + (data.query.pages[i].index - 1)).attr('src', 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Article_icon_cropped.svg/512px-Article_icon_cropped.svg.png');
+                                }
                             }
+                        },
+                        error: function () {
+                            console.log("second call unsuccessful");
                         }
-                    },
-                    error: function () {
-                        console.log("second call unsuccessful");
-                    }
-                })
-
+                    })
+                }
                 // Control UI
-                enableSearchButton();
+                hideSpinner();
             },
             error: function (err) {
                 console.error("error: ", err);
-                enableSearchButton();
+                // Control UI
+                hideSpinner();
             }
         });
     } else {
@@ -172,6 +209,16 @@ $(document).ready(function () {
 
     $('#search').click(function () {
         doSearch();
+    });
+
+    $('#clear').click(function () {
+        clearSearch();
+        disableClearButton();
+        enableSearchButton();
+    });
+
+    $('#random').click(function () {
+        randomSearch();
     });
 
 });
